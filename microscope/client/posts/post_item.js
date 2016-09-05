9ec90d6761147e41c4ab7583140ac1697860b0a2
m381589789 <viporichi@vip.qq.com>
2016-09-05 14:31:25 +0800

    ddd

---
+++ microscope/client/posts/post_item.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/9/5.
+ */
+Template.postItem.helpers({
+    domain: function() {
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    }
+});
---
+++ microscope/client/posts/post_list.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Administrator on 2016/9/5.
+ */
+var postsData = [
+    {
+        title: 'Introducing Telescope',
+        url: 'http://sachagreif.com/introducing-telescope/'
+    },
+    {
+        title: 'Meteor',
+        url: 'http://meteor.com'
+    },
+    {
+        title: 'The Meteor Book',
+        url: 'http://themeteorbook.com'
+    }
+];
+Template.postsList.helpers({
+    posts: postsData
+});
